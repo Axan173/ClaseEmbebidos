8b81c74207e178c3d6678acc5a24ebb120c931c2
Alan Sánchez <alan173sanchez@gmail.com>
2024-02-11 15:38:58 -0600

    CREO Esto es la configuracion

---
+++ Practica_1/MPLAB/Asc_Des_BCD_PIC16f887.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Asc_Des_BCD_PIC16f887.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Asc_Des_BCD_PIC16f887.X.production.hex
