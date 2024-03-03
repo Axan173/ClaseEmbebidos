dad6e786a051de13c3811001def55e61df45ea18
Alan Sánchez <alan.sanchez0639@alumnos.udg.mx>
2024-03-03 17:08:39 -0600

    Codigo Practica 1
    Arreglado apartir del codigo del profe

--- Practica_1.X/newmain.c
+++ Practica_1.X/newmain.c
@@ -14,23 +14,37 @@ void delay1s(void);
 
 void main()
 {
-TRISC=0x00;
-//Configurando el Puerto "C" como Salida.
-OPTION_REG= OPTION_REG & 0x7F; //Activar "Weak Pull-up" ANSELH=0x00;
-//Configurando Entradas Digitales en PORTB.
-PORTB=0XFF;
-//Activando Pull-ups
-while (1) { 
+  static char Eventalarm = 0;
+  
+  TRISC=0x00; //Configurando el Puerto "C" como Salida.
+  OPTION_REG = OPTION_REG & 0x7F; //Activar "Weak Pull-up"
+  ANSELH=0x00; //Configurando Entradas Digitales en PORTB.
+  PORTB = 0xFF; //Activando Pull-ups
+  while (1)
+  {
+        if(PORTB.RB0) //Alarma encendida.
+        {
+                      Eventalarm = (Eventalarm | (PORTB & 0x77));
+                      PORTC = Eventalarm;
+                      //PORTC = PORTB & 0x77; //Enmascaramiento.
+                      delay1s(); //Retardo de 1/4 de segundo.
+                      PORTC = 0x01; //Alarma activada.
+                      delay1s();; //Retardo de 1/4 de segundo.
+        }
+        else
+        {
+            //Portc = 0; //Alarma�Apagado --- version inicial
+            Eventalarm = 0;
+            PORTC = PORTB & 0x76; //Enmascaramiento.             77
+        }
+  }
+}
 
-if(PORTB.RB0) //Alarma encendida.
+void delay1s (void)     
 {
-
-PORTC=PORTB&0x77; //Enmascaramiento.
-delay_ms(250); //Retardo de 1/4 de segundo.
-PORTC=0x01; //Alarma activada.
-delay_ms(250); //Retardo de 1/4 de segundo.
-} 
-else {
-Portc = 0; //Alarma Apagada
-
-}}}
+    long start = 0;
+    long end = 3435; // Esto es .25 Para 59 lineas de ensamblador 
+    //Calibrar simepre xDDDDDD
+    
+    for (long i = start; i <= end; i++);
+}
